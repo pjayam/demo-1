4735aded282cd750dbdfc85e663ed9dadc5ff852
pjayam <42022310+pjayam@users.noreply.github.com>
2024-11-17 19:00:23 +0530

    implemented division (#4)

--- src/main/java/com/crossbridge/toys/practise/MathPractise.java
+++ src/main/java/com/crossbridge/toys/practise/MathPractise.java
@@ -5,10 +5,12 @@ public class MathPractise {
         add(13,20);
         substract(22,13);
         multiply(13,14);
+        division(12,5);
 
         add(10,10,10);
         subtract(100,20,30);
         multiply(11,12,13);
+        division(12,4,3);
     }
 
     public static void add(int a, int b,int c){
@@ -17,11 +19,13 @@ public class MathPractise {
     public static void subtract(int a,int b,int c){
         System.out.println("subtract value = "+  (a-b-c));
     }
-
+    public static void division(int a ,int b) { System.out.println("division value="+ (a/b));}
      public static void multiply(int a,int b,int c){
         System.out.println("multiply value="+ (a*b*c));
      }
-
+     public static void division(int a,int b,int c){
+          System.out.println("division value="+ (a/b/c));
+     }
 
     public static void add(int a, int b){
         System.out.println("Addition value of a="+a + ", b=" +b+" a+b = "+  (a+b));
